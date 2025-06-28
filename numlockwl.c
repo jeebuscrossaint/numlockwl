482577bdcaecf2071cb27bbacaf6cfef6f26eea7
jeebuscrossaint <thediamond270@gmail.com>
2025-06-28 11:43:42 -0400

    cli: fix #1 adding a help flag & print license

--- numlockwl.c
+++ numlockwl.c
@@ -85,6 +85,30 @@ void toggle_numlock(int fd) {
   printf("NumLock toggled.\n");
 }
 
+// Print help information
+void print_help(const char *program_name) {
+  printf("numlockwl - NumLock toggler for Wayland\n");
+  printf("Usage: %s [--on|--off|-h|--help]\n", program_name);
+  printf("Source: https://github.com/jeebuscrossaint/numlockwl\n\n");
+  printf("MIT License\n\n");
+  printf("Copyright (c) 2025 Amarnath P.\n\n");
+  printf("Permission is hereby granted, free of charge, to any person obtaining a copy\n");
+  printf("of this software and associated documentation files (the \"Software\"), to deal\n");
+  printf("in the Software without restriction, including without limitation the rights\n");
+  printf("to use, copy, modify, merge, publish, distribute, sublicense, and/or sell\n");
+  printf("copies of the Software, and to permit persons to whom the Software is\n");
+  printf("furnished to do so, subject to the following conditions:\n\n");
+  printf("The above copyright notice and this permission notice shall be included in all\n");
+  printf("copies or substantial portions of the Software.\n\n");
+  printf("THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\n");
+  printf("IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\n");
+  printf("FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE\n");
+  printf("AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER\n");
+  printf("LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,\n");
+  printf("OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE\n");
+  printf("SOFTWARE.\n");
+}
+
 // Check if device has NumLock capability and LED indicator
 int check_device_has_numlock(struct libevdev *dev) {
   // Check if device has NumLock key and LED
@@ -118,6 +142,13 @@ int main(int argc, char *argv[]) {
       force_on = 1;
     } else if (strcmp(argv[i], "--off") == 0) {
       force_off = 1;
+    } else if (strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "--help") == 0) {
+      print_help(argv[0]);
+      return 0;
+    } else {
+      printf("Unknown option: %s\n", argv[i]);
+      printf("Use -h or --help for usage information.\n");
+      return 1;
     }
   }
 
